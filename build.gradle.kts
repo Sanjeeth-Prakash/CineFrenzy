96bfe913af98a5c348c28385103c2701d0bc662c
Sanjeeth Prakash <sanjeethprakash2004@gmail.com>
2025-07-21 20:31:05 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+}
+
+
+
